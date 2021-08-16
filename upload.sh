93ff18477b737d350081797cfcd4b41f0e03d8af
Lassi Kortela <lassi@lassi.io>
2021-08-16 22:00:00 +0300

    Rename style.css -> schemeorg.css

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 cd "$(dirname "$0")"
-curl --location --fail --silent --show-error -o www/style.css \
-    https://www.staging.scheme.org/style.css
+curl --location --fail --silent --show-error -o www/schemeorg.css \
+    https://www.staging.scheme.org/schemeorg.css
 rsync -vr www/ alpha.servers.scheme.org:/production/containers/www/
