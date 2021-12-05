3fc3a43507a2fe0718a06a0bb97efad17eaf11c7
Ricardo G. Herdt <r.herdt@posteo.de>
2021-12-05 17:13:40 +0100

    run-generation uses .github/workflow as target

--- scripts/run-generation.sh
+++ scripts/run-generation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 script_directory=`dirname $0`
+workflows_path=$script_directory/../.github/workflows/
 
-csi -R r7rs -ss $script_directory/generate-docker-actions.scm $@
+csi -R r7rs -ss $script_directory/generate-docker-actions.scm $workflows_path
