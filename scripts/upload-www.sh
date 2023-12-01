4a8ce963a974e7feae338a9c2db1ec4c192585aa
Lassi Kortela <lassi@lassi.io>
2023-12-01 21:32:33 +0200

    Rename upload.sh -> scripts/upload-www.sh

--- scripts/upload-www.sh
+++ scripts/upload-www.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 cd "$(dirname "$0")"
+cd ..
 curl --location --fail --silent --show-error -o www/schemeorg.css \
     https://www.scheme.org/schemeorg.css
 rsync -vcr www/ tuonela.scheme.org:/production/containers/www/
