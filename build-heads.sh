67c452b17f1f92ddb47a0425b31499aebaa5244d
retropikzel <retropikzel@iki.fi>
2024-10-25 18:01:39 +0300

    Implemented suggested improvements

--- build-heads.sh
+++ build-heads.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-build_only="${1}"
+set -eu
+
+build_only="${1:-}"
+cd "$(dirname "$0")"
+
 implementations=$(find ./implementations -maxdepth 1 -name "*")
 
 for path in $implementations
 do
-    name=$(basename ${path})
-    if [ -d ${path}/head ]
+    name=$(basename "${path}")
+    if [ -d "${path}/head" ]
     then
         if [ "${build_only}" == "" -o "${build_only}" == "${name}" ]
         then
-            docker build ${path}/head --tag=schemers/${name}:head
+            docker build "${path}/head" --tag=schemers/${name}:head
         fi
     fi
 done
