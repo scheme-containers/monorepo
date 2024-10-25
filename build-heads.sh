7208772f97fa826cb8a576d35309fabb941553c8
retropikzel <retropikzel@iki.fi>
2024-10-25 12:13:01 +0300

    Allow building certain implementation

--- build-heads.sh
+++ build-heads.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+build_only="${1}"
 implementations=$(find ./implementations -maxdepth 1 -name "*")
 
 for path in $implementations
@@ -7,7 +8,9 @@ do
     name=$(basename ${path})
     if [ -d ${path}/head ]
     then
-        echo ${path}
-        docker build ${path}/head --tag=schemers/${name}:head
+        if [ "${build_only}" == "" -o "${build_only}" == "${name}" ]
+        then
+            docker build ${path}/head --tag=schemers/${name}:head
+        fi
     fi
 done
