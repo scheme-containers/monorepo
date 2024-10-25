f58348de4f08e68e090952ef7b30b32065566d55
retropikzel <retropikzel@iki.fi>
2024-10-25 18:16:44 +0300

    Implement fixes suggested by shellchekc and shfmt

--- build-heads.sh
+++ build-heads.sh
@@ -7,14 +7,11 @@ cd "$(dirname "$0")"
 
 implementations=$(find ./implementations -maxdepth 1 -name "*")
 
-for path in $implementations
-do
+for path in $implementations; do
     name=$(basename "${path}")
-    if [ -d "${path}/head" ]
-    then
-        if [ "${build_only}" == "" -o "${build_only}" == "${name}" ]
-        then
-            docker build "${path}/head" --tag=schemers/${name}:head
+    if [ -d "${path}/head" ]; then
+        if [ "${build_only}" = "" ] || [ "${build_only}" = "${name}" ]; then
+            docker build "${path}/head" --tag="schemers/${name}:head"
         fi
     fi
 done
