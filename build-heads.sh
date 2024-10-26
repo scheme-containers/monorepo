df2750d8c344ac9b8e4922cf2c56beb3938eda35
Retropikzel <retropikzel@iki.fi>
2024-10-26 11:23:47 +0300

    Added small script to build all heads (#41)

---
+++ build-heads.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+build_only="${1:-}"
+cd "$(dirname "$0")"
+
+implementations=$(find ./implementations -maxdepth 1 -name "*")
+
+for path in $implementations; do
+    name=$(basename "${path}")
+    if [ -d "${path}/head" ]; then
+        if [ "${build_only}" = "" ] || [ "${build_only}" = "${name}" ]; then
+            docker build "${path}/head" --tag="schemers/${name}:head"
+        fi
+    fi
+done
