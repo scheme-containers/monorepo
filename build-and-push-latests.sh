6a7b15b43a21e90454b1317ed7d0aec9552efbf4
retropikzel <retropikzel@iki.fi>
2025-09-23 19:01:24 +0300

    Fixing arm builds

--- build-and-push-latests.sh
+++ build-and-push-latests.sh
@@ -13,7 +13,8 @@ fi
 if [ "${arch}" = "aarch64" ]; then
     implementations=$(echo ${implementations} \
         | sed 's/rhizome-pi//' \
-        | sed 's/ikarus//')
+        | sed 's/ikarus//'
+        | sed 's/loko//')
 fi
 
 for path in $implementations; do
