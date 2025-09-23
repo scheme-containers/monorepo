6fdf90b9e8510b17760019f86f0c1d15a37f29e4
retropikzel <retropikzel@iki.fi>
2025-09-23 19:01:00 +0300

    Fixing arm builds

--- build-and-push-latests.sh
+++ build-and-push-latests.sh
@@ -3,39 +3,39 @@
 set -eu
 cd "$(dirname "$0")"
 
+arch=$(uname --machine)
 implementations=$(find ./implementations -maxdepth 1 -name "*")
+
+if [ "${arch}" = "x86_64" ]; then
+    implementations=$(echo ${implementations} | sed 's/vicare//')
+fi
+
+if [ "${arch}" = "aarch64" ]; then
+    implementations=$(echo ${implementations} \
+        | sed 's/rhizome-pi//' \
+        | sed 's/ikarus//')
+fi
 
 for path in $implementations; do
     name=$(basename "${path}")
     latest_path=$(find "${path}/" -maxdepth 1 -name "*" -not -name "head" | sort | tail -n 1)
     latest_version=$(basename "$latest_path")
 
-    case ${name} in
-        vicare)
-            echo "Skipping ${name} as it does not build"
-            ;;
-        rhizome-pi)
-            echo "Skipping ${name} as it does not build"
-            ;;
-        ikarus)
-            echo "Skipping ${name} as it does not build"
-            ;;
-        *) case ${latest_version} in
-            '' | *[!0-9]*)
-                echo "Latest of ${name} is ${latest_version} which is not valid and won't be built"
-                ;;
-            *)
-                echo "Latest of ${name} is ${latest_version}"
-                echo "Building path ${latest_path}"
-                docker build "${latest_path}" --tag="schemers/${name}:latest"
-                #docker push schemers/${name}:latest
-                #docker build "${latest_path}" --tag="schemers/${name}:${latest_version}"
-                #docker push schemers/${name}:${latest_version}
-                #if [ ! "${build_only}" = "${name}" ]; then
-                    #exit
-                #fi
-                ;;
-            esac
+    case ${latest_version} in
+    '' | *[!0-9]*)
+        echo "Latest of ${name} is ${latest_version} which is not valid and won't be built"
+        ;;
+    *)
+        echo "Latest of ${name} is ${latest_version}"
+        echo "Building path ${latest_path}"
+        docker build "${latest_path}" --tag="schemers/${name}:latest"
+        #docker push schemers/${name}:latest
+        #docker build "${latest_path}" --tag="schemers/${name}:${latest_version}"
+        #docker push schemers/${name}:${latest_version}
+        #if [ ! "${build_only}" = "${name}" ]; then
+            #exit
+        #fi
+        ;;
     esac
 
 done
