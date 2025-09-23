f9bcb8f6dc998e8b8d97596b291d3e4fcea19376
retropikzel <retropikzel@iki.fi>
2025-09-23 18:47:06 +0300

    Fixing arm builds

--- build-and-push-latests.sh
+++ build-and-push-latests.sh
@@ -10,6 +10,15 @@ for path in $implementations; do
     latest_path=$(find "${path}/" -maxdepth 1 -name "*" -not -name "head" | sort | tail -n 1)
     latest_version=$(basename "$latest_path")
 
+    case ${name} in
+        vicare)
+            echo "Skipping ${name} as it does not build"
+            ;;
+        rhizome-pi)
+            echo "Skipping ${name} as it does not build"
+            ;;
+    esac
+
     case ${latest_version} in
     '' | *[!0-9]*)
         echo "Latest of ${name} is ${latest_version} which is not valid and won't be built"
